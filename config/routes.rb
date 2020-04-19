63c6ed14014a4b998b6f496b213231841d54a3b6
nakw <62002086+nakw@users.noreply.github.com>
2020-04-19 22:21:08 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
-  resources :tweets, only: [:index, :show, :new, :destroy, :edit, :update]
+  resources :tweets, only: [:index, :show, :new, :destroy, :edit, :update]do
+   
   resources :comments, only: [:create]
   end
   resources :users, only: [:show]
